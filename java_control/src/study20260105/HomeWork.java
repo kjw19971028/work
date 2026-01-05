004edd3c9f0b25b568c6f3a7ddd477baa5f7a9f6
DW-203 <DW-203@DESKTOP-140NJJF>
2026-01-05 09:10:36 +0900

    숙제

---
+++ java_control/src/study20260105/HomeWork.java
@@ -0,0 +1,39 @@
+package study20260105;
+
+
+import java.util.Scanner;
+
+public class HomeWork {
+    public static void main(String[] args) {
+
+        Scanner kbd = new Scanner(System.in);
+
+        System.out.print("주차 시간을 입력하세요: ");
+        int time = kbd.nextInt();
+
+        int sum= 0;
+        int uptime;
+
+      
+        if (time >= 240)
+        {
+            sum = 2500;
+            uptime = time - 240;
+        } else {
+            sum = 3000;
+            uptime = time - 30;
+        }
+
+        
+        if (uptime < 0) {
+            uptime = 0;
+        }
+
+      
+        if (uptime > 0) {
+            sum += (uptime / 10) * 100;
+        }
+
+        System.out.println("요금은 " + sum + "원 입니다.");
+    }
+}
